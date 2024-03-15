788216d9176ad83bc0d27a0263ba6cab34c676e2
Pedro Lucas <izielucas2016@gmail.com>
2024-03-15 08:04:06 -0300

    puse

--- estruturas-de-dados/arrays/ex2/js/app.js
+++ estruturas-de-dados/arrays/ex2/js/app.js
@@ -19,6 +19,7 @@ function randNumber(min, max){
 let numeroDoDia = randNumber(1, 100)
 console.log(numeroDoDia)
 
+
 btnAposta.addEventListener('click', () => {
     chances.innerHTML = chance
     let numer = numero.value
@@ -29,25 +30,30 @@ btnAposta.addEventListener('click', () => {
         return
     }else{
         if(chance !== 0){
-            if(numer != numeroDoDia){
-                chance--
-                erro++
-                tentativas.push(numer)
-                erros.innerHTML = erro
-                tentar.innerHTML = `[${tentativas}]`
-                chances.innerHTML = chance
-                if(numer < numeroDoDia){
-                    dica.innerHTML = `É um número maior que ${numer}`
-                }else if(numer > numeroDoDia){
-                    dica.innerHTML = `É um número menor que ${numer}`
-                }
-            }else if(numer == numeroDoDia){
+            if(numer == numeroDoDia){
                 ganhou = true
                 alert('Voce Ganhou')
                 dica.innerHTML = `Você ganhou o jogo! O número era: ${numeroDoDia}`
                 btnAposta.disabled = true
+            }else{
+                if(tentativas.indexOf(numer) >= 0){
+                    alert('Você já apostou este número! Tente outro')
+                }else{
+                    chance--
+                    erro++
+                    tentativas.push(numer)
+                    erros.innerHTML = `(${erro}) `
+                    tentar.innerHTML = `${tentativas.join(', ')}`
+                    chances.innerHTML = chance
+                    if(numer < numeroDoDia){
+                        dica.innerHTML = `É um número maior que ${numer}`
+                    }else if(numer > numeroDoDia){
+                        dica.innerHTML = `É um número menor que ${numer}`
+                    }
+                }
             }
             numero.value = ''
+            numero.focus()
         }else{
             alert('Voce Perdeu')
             dica.innerHTML = `Você perdeu o jogo! O número era: ${numeroDoDia}`
