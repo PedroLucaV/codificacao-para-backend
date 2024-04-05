650bdbe3aa22d7ae8f81b884e7b8c86d0c190b7a
Pedro <izielucas2016@gmail.com>
2024-04-05 07:42:59 -0300

    started sync and async studies

--- nodeJS/fundamentos/consoles/index.js
+++ nodeJS/fundamentos/consoles/index.js
@@ -3,3 +3,17 @@ const z = 'Pedro'
 const t = [1, 2, 3]
 
 console.log(a, z, t)
+
+console.count(`O Valor de A ${a}, contagem`)
+console.count(`O Valor de A ${a}, contagem`)
+console.count(`O Valor de A ${a}, contagem`)
+console.count(`O Valor de A ${a}, contagem`)
+console.count(`O Valor de A ${a}, contagem`)
+
+//Mostra quantas vezes este console apareceu
+
+console.log('O nome %s, e ele é bisonho', z)
+
+setInterval(() => {
+    console.clear()
+}, 2000)
